37904042acd98da981c3cddeb880dd1707183f31
David Jahns <imdavidjahns@yahoo.com>
2017-04-24 09:57:04 -0500

    data hooked up to grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,7 +43,11 @@ module.exports = function (grunt) {
         flatten: true,
         partials: ['<%= yeoman.app %>/_includes/**/*.hbs'],
         layoutdir: '<%= yeoman.app %>/_layouts',
-        layout: 'default.hbs'
+        layout: 'default.hbs',
+        data: '<%= yeoman.app %>/_data/*.json'
+      },
+      files: {
+        '<%= yeoman.dist %>/': ['<%= yeoman.app %>/**/*.hbs' ]
       },
       dist: {
         files: [{
