eae928d6a5ee90c3ee7c6a25bf77769fd3e145a1
David Jahns <imdavidjahns@yahoo.com>
2017-09-18 17:16:17 -0500

    added bottom nav and tweaked global things. playing with js to randomize color scheme on load

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,51 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//   console.log('DOMContentLoaded');
-//
-//   var componentsDropdown = document.querySelector('.js-component-dropdown');
-//   var resourcesDropdown = document.querySelector('.js-resources-dropdown');
-//   var appNav = document.querySelector('.js-app-nav');
-//   var appNavToggle = document.querySelector('.js-app-nav-toggle');
-//
-//   componentsDropdown.addEventListener('click', function() {
-//     // console.log('componentsDropdown clicked');
-//     var componentsDropdownList = this.querySelector('.js-component-dropdown-list');
-//
-//     if (this.querySelector('.js-component-dropdown-list.app-menu--hidden')) {
-//       // console.log('menu is hidden, should show the menu now');
-//       componentsDropdownList.classList.remove('app-menu--hidden');
-//       this.querySelector('.app-menu__heading').classList.add('app-menu__heading--open');
-//     } else {
-//       componentsDropdownList.classList.add('app-menu--hidden');
-//       this.querySelector('.app-menu__heading').classList.remove('app-menu__heading--open');
-//       // console.log('menu is visible, should hide the menu now');
-//     }
-//   });
-//
-//   resourcesDropdown.addEventListener('click', function() {
-//     // console.log('resourcesDropdown clicked');
-//     var resourcesDropdownList = this.querySelector('.js-resources-dropdown-list');
-//
-//     if (this.querySelector('.js-resources-dropdown-list.app-menu--hidden')) {
-//       // console.log('menu is hidden, should show the menu now');
-//       resourcesDropdownList.classList.remove('app-menu--hidden');
-//     } else {
-//       resourcesDropdownList.classList.add('app-menu--hidden');
-//       // console.log('menu is visible, should hide the menu now');
-//     }
-//   });
-//
-//   appNavToggle.addEventListener('click', function() {
-//     console.log('appNavToggle clicked');
-//     if (appNav.classList.contains('app-nav--hidden')) {
-//       appNav.classList.remove('app-nav--hidden');
-//       this.classList.add('app-nav__toggle--open');
-//     } else {
-//       appNav.classList.add('app-nav--hidden');
-//       this.classList.remove('app-nav__toggle--open');
-//     }
-//   });
-// });
-
 $( document ).ready(function() {
     if($(window).width() < 1024) {
       $('body').removeClass('hca-layout--has-drawer');
@@ -54,7 +6,6 @@ $( document ).ready(function() {
       $('.hca-menu-toggle__text').html('Menu');
     }
 });
-
 
 $('.hca-menu-toggle').on('click', function(){
   $('.hca-layout__drawer').toggleClass('hca-layout__drawer--is-closed');
@@ -71,7 +22,6 @@ $('.hca-menu-toggle').on('click', function(){
 });
 
 
-
 //Function for offsetting anchor jumps on page.
 // taken from: https://stackoverflow.com/questions/17534661/make-anchor-link-go-some-pixels-above-where-its-linked-to
 
@@ -81,11 +31,26 @@ function offsetAnchor() {
         window.scrollTo(window.scrollX, window.scrollY - 100);
     }
 }
-
 // This will capture hash changes while on the page
 window.addEventListener("hashchange", offsetAnchor);
-
 // This is here so that when you enter the page with a hash,
 // it can provide the offset in that case too. Having a timeout
 // seems necessary to allow the browser to jump to the anchor first.
 window.setTimeout(offsetAnchor, 1); // The delay of 1 is arbitrary and may not always work right (although it did in my testing).
+
+
+// $( document ).ready(function() {
+//   var colors = {
+//     ["hca-background--cadet","hca-background--white"],
+//     ["hca-background--evening","hca-background--morning"],
+//     ["hca-background--ivy","hca-background--mint"],
+//     ["hca-background--hazelnut","hca-background--vanilla"],
+//     ["hca-background--cherry","hca-background--watermelon"]
+//   };
+//   //get random class for app bar
+//   //add to app bar
+//
+//   //get random class for app header
+//   //add to app header
+//
+// });
